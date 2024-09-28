89bc138daefb0b26efd73b5f155cb586c217582c
tmuntaseer06 <t.muntaseer06@gmail.com>
2024-09-28 14:27:47 -0700

    Cursorbullets

--- FallHacks2024/Assets/Scripts/Bullet.cs
+++ FallHacks2024/Assets/Scripts/Bullet.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed;
-    Rigidbody2D body;
+    private Rigidbody2D body;
+
     // Start is called before the first frame update
     void Start()
     {
+        // Get the Rigidbody2D component
         body = GetComponent<Rigidbody2D>();
-        body.velocity = transform.right * speed;
+
+        // Get the mouse position in the world space
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0;  // Ensure the z-axis is 0 for 2D
+
+        // Calculate the direction from the bullet's position to the mouse position
+        Vector2 direction = (mousePosition - transform.position).normalized;
+
+        // Set the velocity of the bullet towards the direction of the mouse click
+        body.velocity = direction * speed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Destroy the bullet after 3 seconds
         Destroy(gameObject, 3);
     }
 }
